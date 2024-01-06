6b6592a82fbb5cecebf66a8fc2ce5e56051ec153
Ksenia1290 <Ksu.tender@yandex.ru>
2024-01-07 00:07:35 +0300

    1

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,16 @@ import './App.css';
 import { Todolist } from './Todolist';
 
 function App() {
+
+    let tasks1=[
+{id:1, title:"HTML&CSS", isDone:true},
+{id:1, title:"JS", isDone:true},
+{id:1, title:"React", isDone:true},
+    ]
+
     return (
         <div className="App">
-        <Todolist title="What to learn"/>
+        <Todolist title="What to learn" tasks={tasks1}/>
        </div>
     );
 }   
