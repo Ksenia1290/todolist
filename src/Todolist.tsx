558a5c78d5793ebd8e373ebb5b6ab1512a35df1a
Ksenia1290 <Ksu.tender@yandex.ru>
2024-01-16 16:02:40 +0300

    1

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -23,11 +23,9 @@ export function Todolist(props: PropsType) {
                         {props.tasks.map((t)=>{
                             return  <li><input type="checkbox" checked={t.isDone}/> 
                             <span>{t.title}</span>
+                            <button onClick={() => {alert(t.id)}}>X</button>
                             </li>
                         })}
-                        <li><input type="checkbox" checked={props.tasks[0].isDone}/> <span>{props.tasks[0].title}</span></li>
-                        <li><input type="checkbox" checked={props.tasks[1].isDone}/> <span>{props.tasks[1].title}</span></li>
-                        <li><input type="checkbox" checked={props.tasks[2].isDone}/> <span>{props.tasks[2].title}</span></li>
                     </ul>
                     <div>
                         <button>All</button>
