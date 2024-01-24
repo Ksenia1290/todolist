c8b6f52f8117e9422e70030199f10a566f844eb8
Ksenia1290 <Ksu.tender@yandex.ru>
2024-01-24 14:37:40 +0300

    1

--- src/App.tsx
+++ src/App.tsx
@@ -21,8 +21,8 @@ let filteredTasks = tasks.filter( t => t.id !==id)
 setTasks(filteredTasks);
 }
 
-function addTask() {
-    let newTask = {id:v1(), title: "New Task", isDone:false}
+function addTask(title:string) {
+    let newTask = {id:v1(), title: title, isDone:false}
     let newTasks = [newTask,...tasks];
     setTasks(newTasks);
 }
