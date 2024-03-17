26aa3ca7b77f32a22ec22e3f61942e086498fcac
Ksenia1290 <Ksu.tender@yandex.ru>
2024-03-17 20:58:32 +0300

    1

--- src/App.tsx
+++ src/App.tsx
@@ -57,11 +57,12 @@ let [allTasks, setAllTasks]=useState({
     todolistId1:[
         {id:v1(), title:"HTML&CSS", isDone:true},
         {id:v1(), title:"JS", isDone:true},
-        {id:v1(), title:"React", isDone:true}],
+        {id:v1(), title:"React", isDone:true}
+    ],
     todolistId2:[
-        {id:v1(), title:"HTML&CSS", isDone:true},
-        {id:v1(), title:"JS", isDone:true},
-        {id:v1(), title:"React", isDone:true}],           
+        {id:v1(), title:"Book", isDone:false},
+        {id:v1(), title:"Milk", isDone:true},
+    ],           
 });
 
     return (
