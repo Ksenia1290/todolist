cbe6e9fc6a9c65efa9f01124b4aff355fe6ea769
Ksenia1290 <Ksu.tender@yandex.ru>
2024-03-17 21:55:34 +0300

    1

--- src/App.tsx
+++ src/App.tsx
@@ -54,12 +54,12 @@ let [todolists,setTodolists]= useState <Array<TodolistType>>([
 ]);
 
 let [allTasks, setAllTasks]=useState({
-    todolistId1:[
+    [todolistId1]:[
         {id:v1(), title:"HTML&CSS", isDone:true},
         {id:v1(), title:"JS", isDone:true},
         {id:v1(), title:"React", isDone:true}
     ],
-    todolistId2:[
+    [todolistId2]:[
         {id:v1(), title:"Book", isDone:false},
         {id:v1(), title:"Milk", isDone:true},
     ],           
