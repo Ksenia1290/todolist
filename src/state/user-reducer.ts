980ed6689275e37d99472653cf2a5dc4226d67d0
Ksenia1290 <Ksu.tender@yandex.ru>
2024-06-09 01:22:15 +0300

    1

--- src/state/user-reducer.ts
+++ src/state/user-reducer.ts
@@ -11,11 +11,16 @@ type ActionType={
 export const userReducer = (state:StateType,action:ActionType)=>{ //reducer  это функция которая принимает state(user) и инструкцию (action)которая говорит как преобразовывать state(user)
 switch(action.type){
     case'INCREMENT-AGE':
-        state.age=state.age+1;
+    let newState={...state}
+        newState.age=state.age+1;
         return state;
     case'INCREMENT-CHILDREN-COUNT':
-        state.childrenCount=state.childrenCount+1;
-        return state;
+return{
+    ...state,
+    childrenCount:state.childrenCount+1
+}
+       // state.childrenCount=state.childrenCount+1;
+      //  return state;
 
     default:
         throw new Error('I dont')
