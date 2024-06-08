4cbb1c1df0c7f825f6bc7e419c10eb58cb5882fc
Ksenia1290 <Ksu.tender@yandex.ru>
2024-06-09 00:30:53 +0300

    1

--- src/state/user-reducer.ts
+++ src/state/user-reducer.ts
@@ -1,3 +1,10 @@
 export const userReducer = (state:any,action:any)=>{ //reducer  это функция которая принимает state(user) и инструкцию (action)которая говорит как преобразовывать state(user)
+switch(action.type){
+    case'BLABLA1':
 
+    case'YO':
+
+    default:
+        throw new Error
+}
 }
