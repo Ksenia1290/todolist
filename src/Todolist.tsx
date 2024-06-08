05df59986f1b17dc863cb8e361cb4a5c50f58271
Ksenia1290 <Ksu.tender@yandex.ru>
2024-06-08 20:53:56 +0300

    1

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -2,7 +2,7 @@ import { ChangeEvent } from "react";
 import { FilterValuesType } from "./App";
 import { AddItemForm } from "./AddItemForm";
 import { EditableSpan } from "./EditableSpan";
-import { Button, IconButton } from "@mui/material";
+import { Button, Checkbox, IconButton } from "@mui/material";
 import { Delete } from "@mui/icons-material";
 
 export type TaskType={
@@ -55,9 +55,8 @@ return(<div>
 
 
 return  <li key={t.id} className={t.isDone? "is-done":''}>
-           <input type="checkbox"
-                  onChange={onChangeStatusHandler}
-                  checked={t.isDone}/> 
+           <Checkbox onChange={onChangeStatusHandler}
+                     checked={t.isDone}/> 
            <EditableSpan title={t.title} 
                          onChange={onChangeTitleHandler}/>
            <IconButton onClick={onClickHandler}>{/*удаление таски иконкой корзина*/}
